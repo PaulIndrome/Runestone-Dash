2c753c02593423b9bcc417220bd76afb889e92f0
Paul Indrome <fpschocke@gmail.com>
2017-12-26 02:47:17 +0100

    main menu
    Built a nice little main menu with a cool DASH animation when clicking the placeholder PLAY button

---
+++ Assets/scripts/systems/MainMenuFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+
+
+public class MainMenuFunctions : MonoBehaviour {
+
+	public delegate void ExampleDelegate();
+
+	public void StartGame(float delay){
+		GetComponent<AudioSource>().Play();
+		if(delay > 0.01f)
+			StartCoroutine(WaitThenCallBack(delay, StartGame));
+		else
+			StartGame();
+	}
+
+	private void StartGame(){
+		SceneManager.LoadScene("ingame_01");
+	}
+
+	private IEnumerator WaitThenCallBack(float delay, ExampleDelegate exDel){
+		yield return new WaitForSeconds(delay);
+		exDel();
+	}
+}
